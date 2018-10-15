25b4ac51727daa87d73d00ae201fc8390de23469
Linkan <Linkan@GREJER>
2018-10-15 19:30:39 +0200

    Add project files.

---
+++ Personlista/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Personlista.Startup))]
+namespace Personlista
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
